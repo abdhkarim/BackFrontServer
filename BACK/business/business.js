578b8ecf9e9d085128047b3ce5c3c06b61079db8
ABDALLAH Karim <karim.abdallah@etu.univ-amu.fr>
2023-04-26 17:12:51 +0200

    first commit

---
+++ BACK/business/business.js
@@ -0,0 +1,62 @@
+// Importation du module dataLayer
+const data = require('../data/dataLayer');
+
+// Définition de constantes pour les valeurs par défaut et limites de pagination
+const defaultNumber = 10;
+const defaultPage = 1;
+const maxNumber = 50;
+
+// Définition de l'objet "business" contenant les méthodes d'accès aux données
+const business = {
+// Méthode pour obtenir tous les clients
+getAllClients : function(){
+return data.getClients();
+},
+// Méthode pour obtenir une page de clients avec un nombre limité de clients par page
+getClients : function(number, page){
+    //initialise les pages si non fait
+    if(page == undefined ){ // == ou === ?????
+        page = defaultPage;
+    }
+    if(number == undefined ){ // == ou === ?????
+        number = defaultNumber;
+    }
+   
+    // Vérification que le nombre de clients par page ne dépasse pas la limite
+    if(number > maxNumber)
+        number = maxNumber;
+
+    // Récupération des clients depuis la couche de données
+    const clients = data.getClients(number, page);
+
+    // Ajout de propriétés supplémentaires à l'objet "clients" (numéro de page, nombre de clients par page, nombre total de pages)
+    clients.page = page;
+    clients.number = number;
+    clients.totalPages = Math.ceil(clients.total / number); //si la division est decimale, renvoie nbr par exces 
+
+    return clients;
+},
+
+// Méthode pour ajouter un utilisateur
+addUser : function(user){
+    data.addUser(user);
+    return { success: true, message: "Utilisateur ajouté avec succès." };
+},
+
+// Méthode pour modifier un utilisateur
+updateUser : function(user){
+    let nb = data.updateUser(user);
+    if(nb) return { success: true, message: "Utilisateur modifié avec succès." };
+    else return { success: false, message: "Erreur lors de la modification du client." };
+},
+
+// Méthode pour supprimer un utilisateur
+removeUser : function(user){
+    let nb = data.removeUser(user);
+    if(nb) return { success: true, message: "Utilisateur supprimé avec succès." };
+    else return { success: false, message: "ID d'utilisateur non trouvé." };
+}
+};
+
+// Exportation de l'objet "business" pour être utilisé dans d'autres fichiers
+module.exports = business;
---
+++ BACK/data/datalayer.js
@@ -0,0 +1,78 @@
+const file = "./data/users.json"; // Chemin du fichier JSON contenant les données des clients
+const fs = require('fs'); // Module pour la lecture et l'écriture de fichiers
+
+let data = {
+    // Récupère tous les clients depuis le fichier JSON
+    getAllClients : function(){
+        const rawdata = fs.readFileSync(file); // Lit le contenu du fichier JSON
+        let clients = JSON.parse(rawdata); // Parse le contenu JSON en un tableau JavaScript
+        return clients; // Retourne le tableau de clients
+    },
+
+    // Récupère un certain nombre de clients à partir d'une page donnée
+    getClients : function(number, page){
+        const rawdata = fs.readFileSync(file); // Lit le contenu du fichier JSON
+        let clients = JSON.parse(rawdata); // Parse le contenu JSON en un tableau JavaScript
+        const total = clients.length; // Nombre total de clients dans le tableau
+        if(number && page){
+            // Découpe le tableau de clients en fonction du nombre et de la page donnée
+            clients = clients.slice((page - 1)*number, page*number);
+        }
+        clients = {
+            total : total,
+            clients : clients
+        };
+        return clients; // Retourne un objet contenant le nombre total de clients et le tableau de clients à afficher
+    },
+
+    // Ajoute un nouvel utilisateur au fichier JSON
+    addUser : function(user){
+        const rawdata = fs.readFileSync(file); // Lit le contenu du fichier JSON
+        let clients = JSON.parse(rawdata); // Parse le contenu JSON en un tableau JavaScript
+        clients.push(user); // Ajoute le nouvel utilisateur à la fin du tableau
+        var newdata = JSON.stringify(clients); // Transforme le tableau JavaScript en JSON
+        fs.writeFile(file, newdata, err => { // Écrit le nouveau contenu dans le fichier JSON
+            if (err) throw err; // En cas d'erreur, affiche l'erreur dans la console
+        });
+    },
+
+    // Met à jour les données d'un utilisateur dans le fichier JSON
+    updateUser : function(user){
+        const data = fs.readFileSync(file); // Lit le contenu du fichier JSON
+        const clients = JSON.parse(data); // Parse le contenu JSON en un tableau JavaScript
+        const objectid = clients.findIndex(obj => obj.id === user.id); // Trouve l'objet utilisateur dans le tableau
+        if (objectid !== -1) { // Si l'objet existe
+            const updatedObject = { ...clients[objectid], ...user }; // Met à jour les propriétés de l'objet avec les nouvelles données
+            clients[objectid] = updatedObject; // Met à jour le tableau avec le nouvel objet mis à jour
+            const updatedData = JSON.stringify(clients, null, 2); // Transforme le tableau JavaScript en JSON avec une indentation de 2 espaces
+            fs.writeFileSync(file, updatedData); // Écrit le nouveau contenu dans le fichier JSON
+            return 1; // Retourne 1 pour indiquer que la mise à jour a réussi
+        } else {
+            return 0; // Retourne 0 pour indiquer que la mise à jour a échoué car l'utilisateur n'a pas été trouvé
+        }
+    },
+
+
+// Cette fonction permet de supprimer un utilisateur à partir de son identifiant
+    removeUser : function(removeuser){
+        // Nous ouvrons le fichier contenant les données des utilisateurs
+        const rawdata = fs.readFileSync(file);
+        // Nous convertissons les données en objet JSON pour pouvoir les manipuler
+        let newclients = JSON.parse(rawdata);
+        // Nous recherchons l'identifiant de l'utilisateur à supprimer dans notre objet clients
+        const id = newclients.findIndex(user => user.id === parseInt(removeuser));
+    // Si l'utilisateur existe dans notre objet clients
+    if (id != -1) {
+        // Nous supprimons l'utilisateur de notre objet clients
+        newclients.splice(id, 1);
+        // Nous écrasons le fichier avec la nouvelle liste d'utilisateurs mise à jour
+        fs.writeFileSync(file, JSON.stringify(newclients, null, 2));
+        // Nous renvoyons la valeur 1 pour signaler que la suppression a été effectuée avec succès
+        return 1;
+    } else
+    // Nous renvoyons la valeur 0 pour signaler que la suppression n'a pas été effectuée car l'utilisateur n'existe pas
+        return 0;
+    }
+    };
+module.exports = data;
+
